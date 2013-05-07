125c9246570e0bc9a685550cf009e951b7278338
Sérgio Michels <sergiomichelss@gmail.com>
2013-05-06 23:49:14 -0300

    Duplicated dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.2.0") {
+              ":release:2.2.1") {
             export = false
         }
     }
